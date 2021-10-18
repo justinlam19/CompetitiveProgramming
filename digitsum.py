2c39455ef935dbc3f25a9b030d9972d1522a6e9a
justinlam19 <88622215+justinlam19@users.noreply.github.com>
2021-10-18 18:00:13 +0100

    Update digitsum.py

--- digitsum.py
+++ digitsum.py
@@ -1,8 +1,16 @@
+# For a pair of integers a and b, the digit sum of the interval 
+# [a,b] is defined as the sum of all digits occurring in all 
+# numbers between (and including) a and b. 
+# For example, the digit sum of [28,31] can be calculated as:
+
+# 2+8+2+9+3+0+3+1=28
+# Given the numbers a and b, calculate the digit sum of [a,b].
+
 from math import log
 
 casenums = int(input())
 
-size = 16
+size = 15
 dp = [0] * (size + 1)
 for i in range(1, size + 1):
 	dp[i] = dp[i - 1] * 10 + 45 * (10 ** (i - 1))
@@ -18,10 +26,6 @@ def digitsum(n):
 	
 	return msd * dp[size] + pwr_ten * (msd - 1) * msd / 2 + msd * (remainder + 1) + digitsum(remainder)
 
-out = []
 for _ in range(casenums):
 	a, b = [int(x) for x in input().split()]
-	out.append(int(digitsum(b) - digitsum(a - 1)))
-
-for i in out:
-	print(i)
+	print(int(digitsum(b) - digitsum(a - 1)))
